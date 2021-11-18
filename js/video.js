a93f25718f8c137a41b8060866f85fe9b195eea8
highfantasy <89871657+highfantasy@users.noreply.github.com>
2021-11-18 18:16:51 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,29 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	console.log(video.currentTime)
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Old video speed is " + video.playbackRate);
+	video.playbackRate -= 0.05;
+	console.log("New video speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Old video speed is " + video.playbackRate);
+	video.playbackRate += 0.05;
+	console.log("New video speed is " + video.playbackRate);
+});
